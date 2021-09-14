953865f5c2af0a152793cbe496a3e354196765d7
Yazdani <yaz4dani@gmail.com>
2021-09-14 22:19:35 +0200

    userProfile

--- router/announcement.js
+++ router/announcement.js
@@ -4,12 +4,10 @@ const { requireLogin } = require("../middleware/auth");
 
 router.post("/announcement",requireLogin, (req, res) => {
   var { des } = req.body;
-
   if (!des) {
     return res.status(400).json({ error: "This field can't be empty" });
   }
   const postData = Announcement({ des,postedBy: req.user });
-
   Announcement.create(postData)
     .then((result) => {
       res.json({ result, message: "Announcement published" });
@@ -18,6 +16,8 @@ router.post("/announcement",requireLogin, (req, res) => {
       console.log(err);
     });
 });
+
+
 router.delete("/deleteannounce/:id", requireLogin, (req, res) => {
   var deleteData = { _id: req.params.id };
   Announcement.findByIdAndDelete(deleteData)
@@ -28,9 +28,6 @@ router.delete("/deleteannounce/:id", requireLogin, (req, res) => {
       console.log(err);
     });
 });
-
-
-
 
 // get all the post
 router.get("/getannouncement", requireLogin, (req, res) => {
@@ -43,4 +40,7 @@ router.get("/getannouncement", requireLogin, (req, res) => {
       console.log(err);
     });
 });
+
+
+
 module.exports = router;
