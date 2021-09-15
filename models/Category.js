9a05f11175109dd9b2024978ab949d2e4ba19b3a
Yazdani <yaz4dani@gmail.com>
2021-09-15 22:19:42 +0200

    cetegory

--- models/Category.js
+++ models/Category.js
@@ -1,10 +1,14 @@
 const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Schema.Types;
 
+
+
 var mongoSchema = mongoose.Schema({
   title: {
     type: String,
   },
+
+
   date: {
     type: String,
     default: Date.now,
