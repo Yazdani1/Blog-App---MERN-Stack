f5ece91ab00847d4c914e9f7f9267478b90df448
Yazdani <yaz4dani@gmail.com>
2021-09-30 13:50:21 +0200

    bootstrapNavigation

--- client/src/Nav.js
+++ client/src/Nav.js
@@ -23,81 +23,110 @@ function Nav() {
   }, []);
 
   const history = useHistory();
-  
+
   const logOut = async () => {
     await localStorage.removeItem("tokenLogin");
     history.push("/signin");
   };
 
   return (
-    <nav className="nav">
-      <ul className="listItems">
-        {localStorage.getItem("tokenLogin") ? (
-          <>
-            <Link to="/" className="eachitem">
-              <li>Home</li>
-            </Link>
-
-            <Link to="/details" className="eachitem">
-              <li>Details</li>
-            </Link>
-
-            <div className="dropdown show">
-              <div
-                className="dropdown-toggle"
-                data-toggle="dropdown"
-                href="#"
-                role="button"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                {user && user.name}
-              </div>
-
-              <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                <Link to="/post" className="dropdown-item each">
-                  Create Post
-                </Link>
-                <Link to="/myPost" className="dropdown-item each">
-                  <li>My Post</li>
-                </Link>
-                <Link to="/postannouncement" className="dropdown-item each">
-                  <li>Announcement</li>
-                </Link>
-                <Link to="/profile" className="dropdown-item each">
-                  Profile
-                </Link>
-
-                <Link to="/profile" className="dropdown-item each">
-                  Dashboard
-                </Link>
-
-                <Link to="/profile" className="dropdown-item each">
-                  Setting
-                </Link>
-                <hr />
-                <Link to="/signin" className="dropdown-item each">
-                  <div onClick={logOut}>Sign Out</div>
-                </Link>
-              </div>
-            </div>
-          </>
-        ) : (
-          <>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+      <a className="navbar-brand" href="#">
+        Blog App
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
           
-            <Link to="/signup" className="eachitem">
-              <li>Sign Up</li>
-            </Link>
-            <Link to="/signin" className="eachitem">
-              <li>Sign In</li>
-            </Link>
+          {localStorage.getItem("tokenLogin") ? (
+            <>
+              <Link to="/" className="eachitem">
+                <li>Home</li>
+              </Link>
 
-            <Link to="/" className="eachitem">
-              <li>Home</li>
-            </Link>
-          </>
-        )}
-      </ul>
+              <Link to="/details" className="eachitem">
+                <li>Details</li>
+              </Link>
+
+              <div className="dropdown show">
+                <div
+                  className="dropdown-toggle"
+                  data-toggle="dropdown"
+                  href="#"
+                  role="button"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  {user && user.name}
+                </div>
+
+                <div
+                  className="dropdown-menu"
+                  aria-labelledby="dropdownMenuLink"
+                >
+                  <Link to="/post" className="dropdown-item each">
+                    Create Post
+                  </Link>
+                  <Link to="/myPost" className="dropdown-item each">
+                    <li>My Post</li>
+                  </Link>
+                  <Link to="/postannouncement" className="dropdown-item each">
+                    <li>Announcement</li>
+                  </Link>
+                  <Link to="/profile" className="dropdown-item each">
+                    Profile
+                  </Link>
+
+                  <Link to="/profile" className="dropdown-item each">
+                    Dashboard
+                  </Link>
+
+                  <Link to="/profile" className="dropdown-item each">
+                    Setting
+                  </Link>
+                  <hr />
+                  <Link to="/signin" className="dropdown-item each">
+                    <div onClick={logOut}>Sign Out</div>
+                  </Link>
+                </div>
+              </div>
+            </>
+          ) : (
+            <>
+              <Link to="/signup" className="eachitem">
+                <li>Sign Up</li>
+              </Link>
+              <Link to="/signin" className="eachitem">
+                <li>Sign In</li>
+              </Link>
+
+              <Link to="/" className="eachitem">
+                <li>Home</li>
+              </Link>
+            </>
+          )}
+
+          {/* <a className="nav-item nav-link active" href="#">
+            Home <span className="sr-only">(current)</span>
+          </a>
+          <a className="nav-item nav-link" href="#">
+            Features
+          </a>
+          <a className="nav-item nav-link" href="#">
+            Pricing
+          </a> */}
+        </div>
+      </div>
     </nav>
   );
 }
