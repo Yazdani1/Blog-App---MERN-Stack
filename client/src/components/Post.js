b3a40535f425b5b1c115b9b12756addd8c1ef81f
Yazdani <yaz4dani@gmail.com>
2021-10-05 21:50:30 +0200

    api design

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -150,4 +150,5 @@ const Post = () => {
     </div>
   );
 };
+
 export default Post;
--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -16,7 +16,6 @@ const Profile = () => {
     });
     setUser(res.data);
   };
-
   useEffect(() => {
     getUser();
   }, []);
