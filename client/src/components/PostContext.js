4358e53f526005f8bd57234dca0b4695c77ceee2
Yazdani <yaz4dani@gmail.com>
2021-10-15 23:18:01 +0200

    context api

--- client/src/components/PostContext.js
+++ client/src/components/PostContext.js
@@ -4,6 +4,65 @@ export const PostContext = createContext();
 
 export const PostProvider = (props)=>{
 
+
+    
+  const [dataItem, setData] = useState([]);
+  const [postsmore, setPosts] = useState([]);
+
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    autoplay: true,
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+
+    const morePost = async () => {
+        await axios
+          .get("/auth/getpost")
+          .then((res) => {
+            setPosts(res.data.resultGet);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      };
+    
+      useEffect(() => {
+      
+        morePost();
+      }, [dataItem]);
+
     return(
         <PostContext.Provider>
             {props.children}
