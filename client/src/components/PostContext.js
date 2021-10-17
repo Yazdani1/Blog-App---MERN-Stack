748a00ba282e74de177156552e6df26383300e0f
Yazdani <yaz4dani@gmail.com>
2021-10-17 23:56:16 +0200

    post context api

--- client/src/components/PostContext.js
+++ client/src/components/PostContext.js
@@ -3,42 +3,6 @@ import React, { useState, useEffect, createContext } from "react";
 export const PostContext = createContext();
 export const PostProvider = (props) => {
   const [allPosts, setPosts] = useState([]);
-  var settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    initialSlide: 0,
-    autoplay: true,
-
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
 
   const allPosts = async (props) => {
     await axios
