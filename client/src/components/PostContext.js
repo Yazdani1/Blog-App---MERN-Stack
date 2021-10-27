9ff34d53ae8413230d1870ae36a83ed8016a778d
Yazdani <yaz4dani@gmail.com>
2021-10-27 22:00:50 +0200

    context api

--- client/src/components/PostContext.js
+++ client/src/components/PostContext.js
@@ -1,21 +1,22 @@
 import React, { useState, useEffect, createContext } from "react";
 
 export const PostContext = createContext();
+
 export const PostProvider = (props) => {
   const [allPosts, setPosts] = useState([]);
 
-  const allPosts = async (props) => {
+  const getallPosts = async (props) => {
     await axios
       .get("/auth/getpost")
       .then((res) => {
         setPosts(res.data.resultGet);
       })
       .catch((err) => {
-        console.log(err);
+        console.log("Error is here"+err);
       });
   };
   useEffect(() => {
-    allPosts();
+    getallPosts();
   }, []);
 
   return (
