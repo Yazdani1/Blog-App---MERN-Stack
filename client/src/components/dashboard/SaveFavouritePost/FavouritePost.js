d39d4f486080f295f1801d9c379bf4dd345c9b44
Yazdani <yaz4dani@gmail.com>
2021-12-08 23:42:30 +0200

    Save Post Feature

--- client/src/components/dashboard/SaveFavouritePost/FavouritePost.js
+++ client/src/components/dashboard/SaveFavouritePost/FavouritePost.js
@@ -1,36 +1,38 @@
-import React,{useEffect,useState,useContext} from "react";
+import React, { useEffect, useState, useContext } from "react";
 
-import {getFavouritePost,removeFavouritePost} from "./ApiFavourite";
+import { getFavouritePost, removeFavouritePost } from "./ApiFavourite";
 import { UserContext } from "../../UserContext";
 
+const FavouritePost = () => {
+  const [wishlist, setWishlist] = useState([]);
+  const [user, setUser] = useContext(UserContext);
 
-const FavouritePost = ()=>{
+  const loadWishlist = () => {
+    getFavouritePost().then((data) => {
+      setWishlist(data);
+      console.log(data);
+    });
+  };
 
-    const[wishlist,setWishlist] = useState([]);
-    const[user,setUser] = useContext(UserContext);
+  useEffect(() => {
+    loadWishlist();
+  }, []);
 
-    const loadWishlist = ()=>{
-        getFavouritePost().then(data=>{
-            setWishlist(data);
-            console.log(data);
-        });
-    }
-
-    useEffect(()=>{
-        loadWishlist();
-    },[])
-
-    return(
-        <div className="container card">
-            <h1>{user && user.name}</h1>
-            <h1>{user && user.email}</h1>
-            <h1>{user && user.about}</h1>
-            <h1>{user && user.experience}</h1>
-            <h1>{user && user.favourite?._id}</h1>
-            {user && user?.favourite?.map(item=>(
-                
-            ))}
-        </div>
-    );
-}
- export default FavouritePost;
+  return (
+    <div className="container card">
+      <h1>{user && user.name}</h1>
+      <h1>{user && user.email}</h1>
+      <h1>{user && user.about}</h1>
+      <h1>{user && user.experience}</h1>
+      <h1>{user && user.favourite?._id}</h1>
+      {user &&
+        user?.favourite?.map((item) => (
+          <>
+            <p>{item._id}</p>
+            <p>{item.des}</p>
+          </>
+        ))}
+    </div>
+  );
+};
+export default FavouritePost;
