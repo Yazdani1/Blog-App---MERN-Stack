d385b82b0f9f0bd54c0e15d93e56a9ceb6d6002b
Yazdani <yaz4dani@gmail.com>
2021-12-11 22:34:06 +0200

    New Feature

--- client/src/components/HomePage/AllpostSection.js
+++ client/src/components/HomePage/AllpostSection.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { getallPosts } from "./Apihomepage";
-import { Spin } from 'antd';
+import { Spin } from "antd";
+import { Link, useHistory, useParams } from "react-router-dom";
+import { FaRegCommentDots } from "react-icons/fa";
+import { AiOutlineLike } from "react-icons/ai";
+import moment from "moment";
+
 
 const AllpostSection = () => {
   const [posts, setPosts] = useState([]);
@@ -21,91 +26,127 @@ const AllpostSection = () => {
 
   //like feature
 
-//   const addlikePost = (id) => {
-//     fetch("/auth/like", {
-//       method: "put",
-//       headers: {
-//         "Content-Type": "application/json",
-//         Authorization: `Bearer ${localStorage.getItem("tokenLogin")}`,
-//       },
-//       body: JSON.stringify({
-//         postId: id,
-//       }),
-//     })
-//       .then((res) => res.json())
-//       .then((result) => {
-//         console.log(result);
+  //   const addlikePost = (id) => {
+  //     fetch("/auth/like", {
+  //       method: "put",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         Authorization: `Bearer ${localStorage.getItem("tokenLogin")}`,
+  //       },
+  //       body: JSON.stringify({
+  //         postId: id,
+  //       }),
+  //     })
+  //       .then((res) => res.json())
+  //       .then((result) => {
+  //         console.log(result);
 
-//         const newItemData = dataItem.map((item) => {
-//           if (item._id == result._id) {
-//             return result;
-//           } else {
-//             return item;
-//           }
-//         });
-//         setData(newItemData);
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       });
-//   };
+  //         const newItemData = dataItem.map((item) => {
+  //           if (item._id == result._id) {
+  //             return result;
+  //           } else {
+  //             return item;
+  //           }
+  //         });
+  //         setData(newItemData);
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //       });
+  //   };
 
   //unlike feature
 
-//   const addunlikePost = (id) => {
-//     fetch("/auth/unlike", {
-//       method: "put",
-//       headers: {
-//         "Content-Type": "application/json",
-//         Authorization: `Bearer ${localStorage.getItem("tokenLogin")}`,
-//       },
-//       body: JSON.stringify({
-//         postId: id,
-//       }),
-//     })
-//       .then((res) => res.json())
-//       .then((result) => {
-//         console.log(result);
-//         const newItemData = dataItem.map((item) => {
-//           if (item._id == result._id) {
-//             return result;
-//           } else {
-//             return item;
-//           }
-//         });
-//         setData(newItemData);
-//       })
-//       .catch((err) => {
-//         console.log(err);
-//       });
-//   };
-
-
-
-
-
+  //   const addunlikePost = (id) => {
+  //     fetch("/auth/unlike", {
+  //       method: "put",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         Authorization: `Bearer ${localStorage.getItem("tokenLogin")}`,
+  //       },
+  //       body: JSON.stringify({
+  //         postId: id,
+  //       }),
+  //     })
+  //       .then((res) => res.json())
+  //       .then((result) => {
+  //         console.log(result);
+  //         const newItemData = dataItem.map((item) => {
+  //           if (item._id == result._id) {
+  //             return result;
+  //           } else {
+  //             return item;
+  //           }
+  //         });
+  //         setData(newItemData);
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //       });
+  //   };
 
   useEffect(() => {
     loadallPosts();
   }, []);
-
 
   if (loading) {
     return (
       <div class="text-center my-5">
         <h1>
           {/* <SyncOutlined spin /> */}
-          <Spin  size="large"/>
+          <Spin size="large" />
         </h1>
       </div>
     );
   }
 
-
-
   return (
     <div className="container">
-      <h1>{JSON.stringify(posts)}</h1>
+      <div className="row">
+        {posts.map((item, index) => (
+          <div className="col-lg-3 col-md-6 col-sm-12" key={index}>
+            <div className="card main-card">
+              <img src={item.photo} className="favpurite-post-image" />
+              <div className="fav-post-text-item">
+                <div className="profile-name-date">
+                  <div className="profile-name-avatar">
+                    <p>{item.postedBy.name.substring(0, 2).toUpperCase()}</p>
+                  </div>
+                  <div className="profile-name-post-date">
+                    <p className="profile-name-size">{item.postedBy.name}</p>
+                    <p>{moment(item.date).format("MMMM Do YYYY")}</p>
+                  </div>
+                </div>
+                <Link
+                  to={"/details/" + item._id}
+                  style={{ textDecoration: "none", color: "black" }}
+                >
+                  <p>{item.title}</p>
+                </Link>
+
+                <div className="like-comments">
+                  <div className="like-button-design">
+                    <div className="like-icons">
+                      <AiOutlineLike size={17} />
+                    </div>
+                    <p className="like-count"> {item.likes?.length} likes</p>
+                  </div>
+
+                  <div className="comment-button-design">
+                    <div className="comment-icons">
+                      <FaRegCommentDots size={17} />
+                    </div>
+                    <p className="comments-count">
+                      {" "}
+                      {item.comments?.length} comments
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- client/src/components/HomePage/TotalPostCount.js
+++ client/src/components/HomePage/TotalPostCount.js
@@ -6,42 +6,49 @@ import { MdCardMembership } from "react-icons/md";
 
 import { Link, useHistory, useParams } from "react-router-dom";
 
-const Totalpostcount = ({totalpost,totaluser}) => {
+const Totalpostcount = ({ totalpost, totaluser }) => {
   return (
-    <div className="container profile_items_container">
+    <div className="container">
       <div className="row">
-        <div className="col-md-4 card home-post-count">
-          <div className="profile-items_design">
-            <p>Total Posts</p>
-            <h4>
-              <BsGraphUp size={25} />
-            </h4>
-            <p>{totalpost}</p>
+        <div className="col-md-4">
+          <div className="card home-post-count">
+            <div className="profile-items_design">
+              <p>Total Posts</p>
+              <h4>
+                <BsGraphUp size={25} />
+              </h4>
+              <p>{totalpost}</p>
+            </div>
           </div>
         </div>
 
-        <div className="col-md-4 card home-post-count">
-          <div className="profile-items_design">
-            <p>Total Users</p>
-            
-            <p>
-              <FaUserSecret size={25} />
-            </p>
+        <div className="col-md-4">
+          <div className="card home-post-count">
+            <div className="profile-items_design">
+              <p>Total Users</p>
 
-            <p>{totaluser}</p>
+              <p>
+                <FaUserSecret size={25} />
+              </p>
+
+              <p>{totaluser}</p>
+            </div>
           </div>
         </div>
 
-        <div className="col-md-4 card home-post-count">
-          <div className="profile-items_design">
-            <p>Become a Mamber Today</p>
-            <p><MdCardMembership size={25}/></p>
-            <Link to="/signup">
-              <button className="btn btn-success">Sign Up</button>
-            </Link>
+        <div className="col-md-4 ">
+          <div className="card home-post-count">
+            <div className="profile-items_design">
+              <p>Become a Mamber Today</p>
+              <p>
+                <MdCardMembership size={25} />
+              </p>
+              <Link to="/signup">
+                <button className="btn btn-success">Sign Up</button>
+              </Link>
+            </div>
           </div>
         </div>
-        
       </div>
     </div>
   );
