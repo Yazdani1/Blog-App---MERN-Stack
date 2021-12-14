5b361c18f9a28ddfb78e0d9ed1816efa76983ad1
Yazdani <yaz4dani@gmail.com>
2021-12-14 18:26:21 +0200

    New File

---
+++ client/src/components/Details Post/LatestpostDetailspage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const LatestpostDetailspage = () => {
+  return (
+    <div>
+      <h1>Latest post details page</h1>
+    </div>
+  );
+};
+export default LatestpostDetailspage;
