7f430018e5dbaa01666849d659e3cd9c52dc799e
Yazdani <yaz4dani@gmail.com>
2021-12-16 22:35:19 +0200

    Similar posts

--- client/src/components/Details Post/SimilarPosts.js
+++ client/src/components/Details Post/SimilarPosts.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getSimilarposts } from "./apidetailsPost";
 
 const SimilarPosts = () => {
+  const [similarposts, setSimilarposts] = useState([]);
+
+  //load similar posts
+  const loadSimilarposts = () => {
+    getSimilarposts().then((data) => {
+      if (data) {
+        setSimilarposts(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadSimilarposts();
+  }, []);
+
   return (
     <div>
       <h1>Similar Posts section</h1>
