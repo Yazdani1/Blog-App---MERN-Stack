907a74891f3ee6b9e5fa5dd15f471171665bd7ef
Yazdani <yaz4dani@gmail.com>
2022-01-01 18:31:33 +0200

    Error Solved

--- client/src/components/PagenotFound.js
+++ client/src/components/PagenotFound.js
@@ -1,14 +1,20 @@
 import React from "react";
 import "../App.css";
 import { MdWifiTetheringErrorRounded } from "react-icons/md";
+import { Link, useHistory, useParams } from "react-router-dom";
 
 const PagenotFound = () => {
   return (
-    <div className="container card">
+    <div className="container">
       <div className="page-not-found">
-          <h5><MdWifiTetheringErrorRounded size={300}/></h5>
-        <h5>Page Not Found.404 Error  <hr/></h5>
-       
+        <Link to="/">
+          <button className="btn btn-primary">Back To Home</button>
+        </Link>
+
+        <h5 className="card noposts-design">
+          <MdWifiTetheringErrorRounded size={300} />
+          Page Not Found.404 Error <hr />
+        </h5>
       </div>
     </div>
   );
