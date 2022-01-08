861044d3c64326d8de7d8892b2fcfcfca55fe250
Yazdani <yaz4dani@gmail.com>
2022-01-08 21:31:28 +0200

    Error Solved

--- client/src/components/HomePage/FirstSection.js
+++ client/src/components/HomePage/FirstSection.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { UserContext } from "../UserContext";
 import "./Homepage.css";
+import { Link } from "react-router-dom";
 
 const FirstSection = () => {
   const [user, setUser] = useContext(UserContext);
@@ -13,11 +14,24 @@ const FirstSection = () => {
             <div className="col-md-12">
               <div class="top-left">
                 <div className="first_Section_data">
-                  <h5>Welcome to this Blog Site</h5>
-                  <p>Learn from others and share your posts with others</p>
-                  <h5>Become a member Today</h5>
-                  <button className="btn btn-success">Sign Up?</button>
-                  <h1>{user && user.name}</h1>
+                  <h5>Welcome to This Multi-User Blog Platform</h5>
+                  <p>Learn from others and share your posts</p>
+                  {!window.localStorage.getItem("tokenLogin") ? (
+                    <>
+                      <h5>
+                        Become a member today and start sharing your posts
+                      </h5>
+                      <Link to={"/signup"}>
+                        <span className="create-account">Create your account</span>
+                      </Link>
+                    </>
+                  ) : (
+                    <Link to="/Dashboard">
+                      <button className="btn btn-primary">
+                        Publish Your Post
+                      </button>
+                    </Link>
+                  )}
                 </div>
               </div>
             </div>
