8251c8688dcfb3ab4fffa4f11519c4f459bdc5f2
Yazdani <yaz4dani@gmail.com>
2022-01-09 23:10:53 +0200

    version 4.4.4

--- models/db.js
+++ models/db.js
@@ -12,31 +12,3 @@ mongoose.connect(process.env.DB,{
         console.log("We got an error"+err);
     }
 });
-
-
-
-// mongoose.connect('mongodb://localhost:27017/mernauth',{
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// },(err)=>{
-//     if(!err){
-//         console.log("Database Connected")
-//     }else{
-//         console.log("We got an error"+err);
-//     }
-// });
-
-
-//this one for online database
-
-// mongoose.connect(process.env.MONGO_URL,{
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-  
-// },(err)=>{
-//     if(!err){
-//         console.log("Database Connected")
-//     }else{
-//         console.log("We got an error"+err);
-//     }
-// })
