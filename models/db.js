95ef92189aa7245c48213fad11ea6b7b506e03f1
Yazdani <yaz4dani@gmail.com>
2022-01-10 01:04:14 +0200

    Error Solved

--- models/db.js
+++ models/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 
-mongoose.connect(process.env.localdb,{
+mongoose.connect(process.env.DB,{
   useNewUrlParser: true,
   useUnifiedTopology: true,
 },(err)=>{
