f95bf8ef9d2045ae4df5962815e185289942ee76
Yazdani <yaz4dani@gmail.com>
2022-01-10 00:46:00 +0200

    Error Solved

--- app.js
+++ app.js
@@ -32,6 +32,15 @@ app.use("/auth", require("./router/profile"));
 //   });
 // }
 
+if(process.env.NODE_ENV=='production'){
+  const path = require('path')
+
+  app.get('/',(req,res)=>{
+      app.use(express.static(path.resolve(__dirname,'client','build')))
+      res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+  })
+}
+
 app.listen(PORT, (req, res) => {
   console.log("Server connected");
 });
