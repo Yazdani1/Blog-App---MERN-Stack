aa6dfe9709891c48189d24061bd658caf2241265
Yazdani <yaz4dani@gmail.com>
2022-01-10 01:18:56 +0200

    Error SOlved

--- app.js
+++ app.js
@@ -35,14 +35,14 @@ app.use("/auth", require("./router/profile"));
 
 //to deploy vercel
 
-if(process.env.NODE_ENV=='production'){
-  const path = require('path')
+// if(process.env.NODE_ENV=='production'){
+//   const path = require('path')
 
-  app.get('/',(req,res)=>{
-      app.use(express.static(path.resolve(__dirname,'client','build')))
-      res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-  })
-}
+//   app.get('/',(req,res)=>{
+//       app.use(express.static(path.resolve(__dirname,'client','build')))
+//       res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+//   })
+// }
 
 app.listen(PORT, (req, res) => {
   console.log("Server connected");
